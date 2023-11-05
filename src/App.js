a55f9c713ffdc13f1fb68eaf7e1524f765ff8951
danielawde <danielawde9@gmail.com>
2023-11-05 12:27:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import LanguageProvider from "./LanguageProvider";
+import ThemeProvider from "./ThemeProvider";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LanguageProvider>
+      <ThemeProvider>
+        <Navbar />
+        <Header />
+        <Footer />
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
 
